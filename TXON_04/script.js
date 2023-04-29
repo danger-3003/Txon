003602388658af81ec1792539f5501befbac44ed
Sumanth Narem <120869569+danger-3003@users.noreply.github.com>
2023-04-29 09:04:23 +0530

    Add files via upload

---
+++ TXON_04/script.js
@@ -0,0 +1,10 @@
+function scroll_left()
+{
+    var left=document.querySelector(".project");
+    left.scrollBy(300,0)
+}
+function scroll_right()
+{
+    var right=document.querySelector(".project");
+    right.scrollBy(-300,0)
+}
